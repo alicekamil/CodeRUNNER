577b5c8659a4657293d67e365acbca90706fd5f5
alicekamil <alice.anastasiak@outlook.com>
2023-04-18 11:43:06 +0200

    First Commit, created LevelManager and Platform class

---
+++ Source/CodeRunneR/CodeRunneR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CodeRunneR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CodeRunneR, "CodeRunneR" );
---
+++ Source/CodeRunneR/Private/CPPLevelManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPLevelManager.h"
+
+// Sets default values
+ACPPLevelManager::ACPPLevelManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPPLevelManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPPLevelManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
